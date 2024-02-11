f561a069ae72e1a1649c814d5ed837bc6859ae04
EimanEltayeb <em_moh092@hotmail.com>
2024-02-11 02:23:03 -0800

    g

--- console.py
+++ console.py
@@ -186,13 +186,14 @@ class HBNBCommand(cmd.Cmd):
             ln = f"{cls_[0]} {idd}"
             self.do_destroy(ln)
             return
-        if cls_[1].startswith('update'):
+        if cls_[1].startswith('update') and not cls_[1].endsswith('}'):
             idd = cls_[1].split('"')[1]
             att = cls_[1].split('"')[3]
             val = cls_[1].split('"')[5]
             ln = f"{cls_[0]} {idd} {att} \"{val}\""
             self.do_update(ln)
             return
+        
 
 
             
